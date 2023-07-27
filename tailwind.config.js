0597f292e866c71265df100832acfac0b259d87b
Sayed Jilliyan <netslayer71@gmail.com>
2023-07-28 01:45:00 +0700

    add

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+};
+
+// npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
